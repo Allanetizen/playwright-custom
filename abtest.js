d7382565e343a93926eeb3ccf0f1d2bb22ba655e
Allan <allankosgei747@gmail.com>
2022-03-08 11:45:09 +0300

    initialized and tested abtest page

---
+++ abtest.js
@@ -0,0 +1,23 @@
+const playwright = require('playwright');
+
+(async ()=>{
+
+    //code execution here
+    const browser = await playwright["chromium"].launch({
+        headless: false
+    }    )
+
+    //context
+    const context = await browser.newContext();
+
+
+
+    //page
+    const page = await context.newPage();
+
+    //navigate to page
+    await page.goto("https://the-internet.herokuapp.com/abtest")
+
+
+
+})();
