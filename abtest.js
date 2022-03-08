9a7222f62d6177ca0e39bd7e88683dd26508f097
Allan <allankosgei747@gmail.com>
2022-03-08 12:48:05 +0300

    text selection

--- abtest.js
+++ abtest.js
@@ -1,27 +1,28 @@
-const playwright = require('playwright');
+const playwright = require("playwright");
 
-(async ()=>{
+(async () => {
+  //code execution here
+  const browser = await playwright["chromium"].launch({
+    headless: false,
+  });
 
-    //code execution here
-    const browser = await playwright["chromium"].launch({
-        headless: false
-    }    )
+  //context
+  const context = await browser.newContext();
 
-    //context
-    const context = await browser.newContext();
+  //page
+  const page = await context.newPage();
 
+  //navigate to page
+  await page.goto("https://the-internet.herokuapp.com/abtest");
+  //screenshot
+  // await page.screenshot({path: `ea-${Date.now}.png`})
+  //testing for the page text
+  // Click text=A/B Test Control
+  await page.locator('.example').click();
+  // Click text=Also known as split testing.
+  await page.locator('').click();
 
-
-    //page
-    const page = await context.newPage();
-
-    //navigate to page
-    await page.goto("https://the-internet.herokuapp.com/abtest")
-    //screenshot
-    await page.screenshot({path: `ea-${Date.now}.png`})
-    //close
-    await browser.close();
-
-
-
+  await expect(page.locator(".example")).toHaveText("A/B Test Control");
+  //close
+  await browser.close();
 })();
